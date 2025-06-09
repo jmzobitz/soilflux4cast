6e94d3d38e2060c2aac19a12291a41f42b0f9dfe
John Zobitz <zobitz@augsburg.edu>
2025-06-09 07:36:22 -0400

    update of files to correct.

--- scripts/daily_prediction.R
+++ scripts/daily_prediction.R
@@ -19,14 +19,14 @@ null_model <- function(TSOIL) {
 }
 
 # Compute forecast
-input_forecast <- readr::read_csv(input_date) |>
+input_forecast <- readr::read_csv(input_file) |>
   mutate(model = 'null',
          value = null_model(TSOIL)) |>
   select(date,site,forecast,horizon,model,value)
 
 
 # Determine the date of forecast
-forecast_date <- input_date |> str_extract(pattern = "[:digit:]{4}-[:digit:]{2}-[:digit:]{2}(?=.csv)")
+forecast_date <- input_file |> str_extract(pattern = "[:digit:]{4}-[:digit:]{2}-[:digit:]{2}(?=.csv)")
 
 forecast_file <- paste0('data/outputs/forecast_prediction-',forecast_date)
 
