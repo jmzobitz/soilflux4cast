ba77931b541f4f4b74a319ac864e870da4e7e66e
John Zobitz <zobitz@augsburg.edu>
2025-06-11 12:47:44 -0400

    more cleanup.

--- scripts/daily_prediction.R
+++ scripts/daily_prediction.R
@@ -5,27 +5,59 @@ library(tidyr)
 library(stringr)
 
 args <- commandArgs(trailingOnly = TRUE)
-input_file <- args[1]  
+forecast_date <- args[1]  
+
+
+
+# Acquire the NEON site_data 
+site_data <- readr::read_csv(paste0("https://raw.githubusercontent.com/eco4cast/neon4cast-targets/","main/NEON_Field_Site_Metadata_20220412.csv"),show_col_types = FALSE) |> 
+  dplyr::filter(terrestrial == 1)|> 
+  dplyr::select(field_site_id,field_latitude,field_longitude)
+
 
 
 # Define null model to create soil values
 null_model <- function(TSOIL) {
   
-  R_S <- (0.598 + exp(0.044*(TSOIL-273.15)))
+  # Globbed from bigleaf R package scripts
+  # molar mass of carbon (kg mol-1) = 0.012011
+  # conversion micromole (umol) to mole (mol) = 1e-06
+  # conversion kilogram (kg) to gram (g) = 1000
+  # seconds per day = 86400
+  
+  conv <- 1e-6 * 0.012011 * 1000 * 86400
+  
+  R_S <- (0.598 + exp(0.044*(TSOIL-273.15)))*conv  # Convert to gC m-2 d-1
   
   return(R_S)
 }
 
+bucket <- "bio230014-bucket01"
+path <- "neon4cast-drivers/noaa/gefs-v12/stage1"
+endpoint <- "https://sdsc.osn.xsede.org"
+
+stage1 <- 
+  glue::glue("{bucket}/{path}/reference_datetime={forecast_date}") |>
+  arrow::s3_bucket(endpoint_override = endpoint, anonymous = TRUE) |>
+  arrow::open_dataset()
+
+
+env_vars <- c("PRES","TSOIL", "SOILW", "WEASD", "SNOD","ICETK")
+
+# Grab the data we need
+forecast_data <- stage1 |> 
+  filter(variable %in% env_vars,
+         site_id %in% site_data$field_site_id) |> 
+  collect() |>
+  mutate(prediction = if_else(prediction ==9999.00,NA,prediction)) |>
+  pivot_wider(names_from = "variable",values_from = "prediction")
+
+
 # Compute forecast
-input_forecast <- readr::read_csv(input_file) |>
+input_forecast <- forecast_data |>
   mutate(model = 'null',
          value = null_model(TSOIL)) |>
-  select(date,site,forecast,horizon,model,value)
-
-
-# Determine the date of forecast
-forecast_date <- input_file |> 
-  stringr::str_extract(pattern = "[:digit:]{4}-[:digit:]{2}-[:digit:]{2}(?=.csv)")
+  select(-all_of(env_vars))
 
 forecast_file <- paste0('data/outputs/forecast_prediction-',forecast_date,'.csv')
 
