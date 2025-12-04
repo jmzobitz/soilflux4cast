6f2e2b72a15af11997f1e427b64605088fc7193c
John Zobitz <zobitz@augsburg.edu>
2025-12-03 21:22:01 -0600

    update for 2 new models

--- scripts/daily_prediction.R
+++ scripts/daily_prediction.R
@@ -4,11 +4,19 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 library(purrr)
+library(xgboost)
 
 args <- commandArgs(trailingOnly = TRUE)
 forecast_date <- args[1]  
 
-year_before <- as.character(as.numeric(substr(forecast_date,1,4))-1)
+# month_before <- sprintf("%02d",as.numeric(substr(forecast_date,6,7))-1)
+# year_ref <- substr(forecast_date,1,4)
+# 
+# # Adjust if the month is 00 - we need to look one more back
+# if (month_before == "00") {
+#   month_before <- "12"
+#   year_ref <- as.character(as.numeric(year_before)-1)
+# }
 
 # Acquire the NEON site_data 
 site_data <- readr::read_csv(paste0("https://raw.githubusercontent.com/eco4cast/neon4cast-targets/","main/NEON_Field_Site_Metadata_20220412.csv"),show_col_types = FALSE) |> 
@@ -143,16 +151,142 @@ input_forecast_exp <- driver_data |>
   mutate(model = 'exp') |>
   select(-all_of(env_vars))
   
+#### For models 4 and 5 we need to determine the different months first
+#### Set the month to download the data and values
+curr_month <- driver_data$datetime |>
+  min() |>
+  month()
 
-# next if we have forecast drivers we compute them
+curr_year <- driver_data$datetime |>
+  min() |>
+  year()
+
+curr_day <- driver_data$datetime |>
+  min() |>
+  day()
+
+if(curr_day < 15) {
+  curr_month <- curr_month - 2 # Go two months back
+} else {
+  curr_month <- curr_month - 1
+}
+
+if (curr_month == 0) {
+  curr_month <- 12
+  curr_year <- curr_year - 1
+}
+
+if (curr_month == -1) {
+  curr_month <- 11
+  curr_year <- curr_year - 1
+}
 
 
+drivers_month <- download_annual_values("drivers",curr_year,month = curr_month)
+targets_month <- download_annual_values("targets",curr_year,month = curr_month)
+# Acquire and download the model and
+
+joined_data_month <- drivers_month |>
+  inner_join(targets_month,by=c("site_id","startDateTime")) |>
+  rename(TSOIL = SOILT,
+         SOILW = VSWC) |>
+  group_by(site_id) |>
+  nest() |>
+  rename(param_data = data)
+
+# We can use this for each month - shared between each model
+nested_model_info <- driver_data |>
+  mutate(TSOIL = TSOIL-273.15) |>
+  group_by(site_id) |>
+  nest() |>
+  rename(eval_data = data) |>
+  inner_join(joined_data_month,by="site_id")
+
+
+# # model 4: fit on a month
+# 
+# drivers_month <- download_annual_values("drivers",year_ref,month = month_before)
+# targets_month <- download_annual_values("targets",year_ref,month = month_before)
+# 
+# # now join by site and day
+# 
+# joined_data_month <- drivers_month |>
+#   inner_join(targets,by=c("site_id","startDateTime")) |>
+#   group_by(site_id) |>
+#   nest() |>
+
+#   select(-data)
+# 
+# 
+ input_forecast_exp_month <- nested_model_info |>
+   mutate(
+     param_data_temp = map(.x=param_data,.f=~(.x |> rename(SOILT=TSOIL) ) ),
+     fit_coeff = map(param_data_temp,fit_exp_soilR)) |>
+   select(-param_data_temp) |>
+   mutate(fit_value = map2(.x=fit_coeff,.y=eval_data,.f=~calc_soilR(.x,.y))) |>
+   select(site_id,fit_value) |>
+   unnest(cols=c(fit_value)) |>
+   mutate(model = 'exp_month') |>
+   select(-all_of(env_vars))
+
+## Model 5 - xgboost!
+
+# input_param_data is what we use to parameterize the model
+# input_driver_data is what we use to evaluate the model
+compute_xgboost <- function(input_param_data,input_eval_data) {
+  
+  # Convert to matrix
+  X <- input_param_data |> select(TSOIL,SOILW) |>
+    as.matrix()
+  
+  Y <- input_param_data |> pull(flux)
+  
+  
+  dtrain <- xgb.DMatrix(data = X, label = Y)
+  
+  model <- xgboost(
+    data = dtrain,
+    objective = "reg:squarederror",
+    max_depth = 3,
+    eta = 0.1,
+    nrounds = 200,
+    verbose = 0
+  )
+  
+  
+  eval_matrix <- input_eval_data |>
+    select(TSOIL,SOILW) |>
+    as.matrix()
+  
+  # Predictions
+  input_eval_data$value <- predict(model, eval_matrix)
+  
+  return(input_eval_data)
+  
+  
+}
+
+# now join by site and day
+
+# Now ready to map!
+
+input_forecast_xg <- nested_model_info |>
+  mutate(fit_value = map2(.x=param_data,.y=eval_data,.f=~compute_xgboost(.x,.y))) |>
+  select(site_id,fit_value) |>
+  unnest(cols=c(fit_value)) |>
+  mutate(model = 'xgboost') |>
+  select(-all_of(env_vars))
 
 
 forecast_file <- paste0('data/outputs/forecast_prediction-',forecast_date,'.csv')
 
 # glob forecasts together
-input_forecast <- rbind(input_forecast_null,input_forecast_lm,input_forecast_exp)
+input_forecast <- rbind(input_forecast_null,
+                        input_forecast_lm,
+                        input_forecast_exp,
+                        input_forecast_exp_month,
+                        input_forecast_xg
+                        )
 
 # Write updated data back to CSV
 write_csv(input_forecast, file = forecast_file)
