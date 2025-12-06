1b66eba7738af3983c3656c21c7fee13f5451b86
John Zobitz <zobitz@augsburg.edu>
2025-12-06 11:16:06 -0600

    update to see if this works.

--- scripts/get_noaa_month.R
+++ scripts/get_noaa_month.R
@@ -16,29 +16,36 @@ all_dates  <- seq.Date(start_date, end_date, by = "day")
 # ----- Your namespaced function (inserted from earlier work) -----
 get_nooa_vars <- function(forecast_date) {
   
-  ### Example variable initialization
-  # stage1 <- arrow::open_dataset(...)   # <--- YOUR SOURCE
-  # env_vars <- c("tmp2m", "rh2m", ...)  # <--- YOURENV VARIABLES
   
   site_data <- readr::read_csv(
     "https://raw.githubusercontent.com/eco4cast/neon4cast-targets/main/NEON_Field_Site_Metadata_20220412.csv",
     show_col_types = FALSE
-  ) %>%
-    dplyr::filter(terrestrial == 1) %>%
+  ) |>
+    dplyr::filter(terrestrial == 1) |>
     dplyr::select(field_site_id, field_latitude, field_longitude)
   
-  driver_data <- stage1 %>%                            # <-- must already exist in your environment!
+  bucket <- "bio230014-bucket01"
+  path <- "neon4cast-drivers/noaa/gefs-v12/stage1"
+  endpoint <- "https://sdsc.osn.xsede.org"
+  
+  stage1 <- 
+    glue::glue("{bucket}/{path}/reference_datetime={forecast_date}") |>
+    arrow::s3_bucket(endpoint_override = endpoint, anonymous = TRUE) |>
+    arrow::open_dataset()
+  
+  env_vars <- c("PRES","TSOIL", "SOILW", "WEASD", "SNOD","ICETK")
+  
+  driver_data <- stage1 |>   
     dplyr::filter(
       variable %in% env_vars,
       site_id %in% site_data$field_site_id
-    ) %>%
-    dplyr::collect() %>%
-    dplyr::mutate(prediction = dplyr::if_else(prediction == 9999.00, NA, prediction)) %>%
-    tidyr::pivot_wider(names_from = variable, values_from = prediction) %>%
-    dplyr::filter(ensemble == "gec00", horizon < 86400) %>%
-    dplyr::mutate(datetime = lubridate::floor_date(datetime, unit = "day")) %>%
-    dplyr::filter(datetime == forecast_date) %>%
-    dplyr::group_by(site_id, datetime) %>%
+    ) |>
+    dplyr::collect() |>
+    dplyr::mutate(prediction = dplyr::if_else(prediction == 9999.00, NA, prediction)) |>
+    tidyr::pivot_wider(names_from = variable, values_from = prediction) |>
+    dplyr::filter(ensemble == "gec00", horizon < 86400) |>
+    dplyr::mutate(datetime = lubridate::floor_date(datetime, unit = "day")) |>
+    dplyr::group_by(site_id, datetime) |>
     dplyr::summarize(
       dplyr::across(all_of(env_vars), ~ mean(.x, na.rm = TRUE)),
       .groups = "drop"
@@ -51,7 +58,7 @@ get_nooa_vars <- function(forecast_date) {
 results <- purrr::map_df(all_dates, get_nooa_vars)
 
 # ----- Write combined CSV -----
-out_dir  <- "data/drivers"
+out_dir  <- "data/drivers/noaa"
 dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
 
 outfile <- file.path(out_dir, paste0("noaa_stage1_", month_input, ".csv"))
