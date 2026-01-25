5160cadccda4a6ae87f4056882996aed5fd768ca
John Zobitz <zobitz@augsburg.edu>
2026-01-25 12:07:32 -0600

    update of paralle site workflow and code.

--- scripts/process_single_site.R
+++ scripts/process_single_site.R
@@ -11,6 +11,7 @@ suppressPackageStartupMessages({
   library(tidyverse)
   library(neonUtilities)
   library(lubridate)
+  library(zoo)
 })
 
 message("[process_single_site] site=", site_id, " month=", curr_month)
@@ -21,8 +22,86 @@ flux_dir <- file.path("data", "targets", "neon")
 if (!dir.exists(env_dir)) dir.create(env_dir, recursive = TRUE)
 if (!dir.exists(flux_dir)) dir.create(flux_dir, recursive = TRUE)
 
-# conversion factor
-conv <- 1e-6 * 0.012011 * 1000 * 1800
+### Define some auxiliary functions
+weighted_mean_sd <- function(input_compute) {
+  ### Purpose: this function computes the weighted mean of flux and flux_err.
+  # set default vals
+  out_val <- tibble(mean = NA,err = NA)
+  
+  # Combine these as a data table, remove NA for each measurement pair
+  input <- input_compute |>
+    select(flux,flux_err) |>
+    drop_na()
+  
+  if(nrow(input > 0)) {
+    
+    weights <- 1/(input$flux_err)^2
+    vals <- input$flux
+    
+    mean <- sum(weights*vals)/sum(weights)
+    err <- sqrt(1/sum(weights))
+    
+    out_val <- tibble(mean,err)
+  }
+  
+  return(out_val)
+  
+}
+
+
+flux_daily_agg <- function(input_fluxes) {
+  ## The input is a nested data table at each location and halfhour.  We need to average across the methods and sites.
+  
+  # Globbed from bigleaf R package scripts
+  # https://www.swissfluxnet.ethz.ch/index.php/documentation/conversion-sequences/
+  # Go to umol CO2 m-2 s-1 → g C m-2 30min-1
+  # molar mass of carbon (kg mol-1) = 0.012011
+  # conversion micromole (umol) to mole (mol) = 1e-06
+  # conversion kilogram (kg) to gram (g) = 1000
+  # seconds per half hour = 1800
+  
+  conv <- 0.02161926
+  
+  # First compute the average across all flux methods at the startDateTime and the horizontalPosition
+  
+  avg_by_time_hor <- input_fluxes |> 
+    mutate(flux = map(.x=flux_compute,.f=~weighted_mean_sd(.x)) ) |> 
+    select(startDateTime,horizontalPosition,flux) |>
+    unnest(cols=c(flux)) |>
+    ungroup() |>
+    rename(flux = mean,
+           flux_err = err)
+  
+  
+  ### Next Group by start date time
+  avg_by_time <- avg_by_time_hor |>
+    group_by(startDateTime) |>
+    nest() |>
+    mutate(flux = map(.x=data,.f=~weighted_mean_sd(.x)) ) |>
+    select(startDateTime,flux) |>
+    unnest(cols=c(flux)) |>
+    ungroup() |>
+    rename(flux = mean,
+           flux_err = err) |>
+    mutate(flux = zoo::na.approx(flux),  #Use zoo package to do linear interpolation for any NAs:
+           flux_err = zoo::na.approx(flux_err))
+  
+  
+  ### Now aggregate up to daily values
+  avg_day <- avg_by_time |>
+    mutate(day = floor_date(startDateTime,unit="day"),
+           flux = flux*conv,
+           flux_err = flux*conv) |>
+    group_by(day) |>
+    summarize(flux = sum(flux,na.rm=TRUE),
+              flux_err = sqrt(sum(flux_err^2,na.rm=TRUE))  # Standard flux propogation
+    )
+  
+  return(avg_day)
+}
+
+
+
 
 try({
   out_env_data <- acquire_neon_data(
@@ -59,34 +138,16 @@ try({
   message("[process_single_site] wrote env: ", env_out_file)
   
   # flux
-  flux_full <- compute_neon_flux(
+  flux_input <- compute_neon_flux(
     input_site_env = out_env_data$site_data,
     input_site_megapit = out_env_data$site_megapit
-  ) |>
-    select(-any_of("surface_diffusivity")) |>
-    unnest(cols = c(flux_compute)) |>
-    # group_by(startDateTime) |>
-    # mutate(
-    #   flux = flux * conv,
-    #   flux_err = flux_err * conv
-    # ) |>
-    # summarize(
-    #   flux_out = mean(flux, na.rm = TRUE),
-    #   flux_err = sd(flux, na.rm = TRUE) / sqrt(sum(!is.na(flux))),
-    #   .groups = "drop"
-    # ) |>
-    # rename(flux = flux_out) |>
-    group_by(startDateTime = floor_date(startDateTime, unit = "day")) |>
-    summarize(
-      flux = mean(flux, na.rm = TRUE),   # do average daily flux (for now)
-      #flux_err = sqrt(sum(flux_err^2, na.rm = TRUE)),
-      flux_err = mean(flux_err, na.rm = TRUE), 
-      .groups = "drop"
-    ) |>
+  )
+  
+  flux_full <- flux_daily_agg(flux_input) |>
     ungroup() |>
-    mutate(site_id = site_id) |>
+    mutate(site_id = curr_site_name) |>
     relocate(site_id) |>
-    arrange(startDateTime)
+    rename(startDateTime = day)
   
   flux_out_file <- file.path(flux_dir, paste0("forecast_prediction-", site_id, "-", curr_month, ".csv"))
   readr::write_csv(flux_full, flux_out_file)
